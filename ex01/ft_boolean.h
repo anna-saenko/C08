1c3ccbb36270c3548514a47dedf80445ef2c7402
Anna Saenko <asaenko@c2r3s5.42luxembourg.lu>
2023-12-09 16:48:39 +0100

    01

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaenko <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 16:19:47 by asaenko           #+#    #+#             */
+/*   Updated: 2023/12/09 16:48:20 by asaenko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define TRUE 1
+# define FALSE 0
+# define EVEN(x) !(x % 2)
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int	t_bool;
+
+#endif
