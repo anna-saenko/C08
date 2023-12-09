b75c5de77cb980a7d4bc38ac13a835eaf58e6087
Anna Saenko <asaenko@c2r3s5.42luxembourg.lu>
2023-12-09 16:35:31 +0100

    00

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,12 +6,15 @@
 /*   By: asaenko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 16:16:56 by asaenko           #+#    #+#             */
-/*   Updated: 2023/12/09 16:17:40 by asaenko          ###   ########.fr       */
+/*   Updated: 2023/12/09 16:34:44 by asaenko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
 
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
+#endif
