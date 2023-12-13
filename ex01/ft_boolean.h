a3cff067248d61b9d85ede44e918f408c1fa73c5
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-12-13 12:08:19 +0100

    fixed

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -15,13 +15,12 @@
 
 # include <unistd.h>
 
+typedef int	t_bool;
 # define TRUE 1
 # define FALSE 0
 # define EVEN(number) (number % 2 == 0)
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
-
-typedef int	t_bool;
 
 #endif
