024e90f578fc59fdc951a368a769fc9417aac4ee
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-12-13 12:05:35 +0100

    fixed

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -17,7 +17,7 @@
 
 # define TRUE 1
 # define FALSE 0
-# define EVEN(x) !(x % 2)
+# define EVEN(number) (number % 2 == 0)
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
